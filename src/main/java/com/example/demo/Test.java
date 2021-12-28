9c737949d55e960bd35fb38770630fa871d74d6c
liuyanqun1 <liuyanqun1@jd.com>
2021-12-28 13:25:46 +0800

    提交

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -1,31 +1,31 @@
-package com.example.demo;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-/**
- * @description: TODO 类描述
- * @author: liuyanqun1
- * @createTime 2021年12月18日 19:06:00
- **/
-public class Test {
-
-    public static void main(String[] args) {
-        List<Long> list = new ArrayList<>();
-        list.add(11L);
-        System.out.println(list.stream().limit(10));
-
-        list.stream();
-
-        Integer a = 1;
-        Integer b = 2;
-        Integer c = null;
-
-        Boolean falg =false;
-        Integer resut = falg?a*b:c;
-        System.out.println(resut);
-
-
-    }
-}
+//package com.example.demo;
+//
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//
+///**
+// * @description: TODO 类描述
+// * @author: liuyanqun1
+// * @createTime 2021年12月18日 19:06:00
+// **/
+//public class Test {
+//
+//    public static void main(String[] args) {
+//        List<Long> list = new ArrayList<>();
+//        list.add(11L);
+//        System.out.println(list.stream().limit(10));
+//
+//        list.stream();
+//
+//        Integer a = 1;
+//        Integer b = 2;
+//        Integer c = null;
+//
+//        Boolean falg =false;
+//        Integer resut = falg?a*b:c;
+//        System.out.println(resut);
+//
+//
+//    }
+//}
